d67079bc55ce91ef34b0bb1da16c3d1abf88b6ef
shihenw <shihenw@cmu.edu>
2016-07-18 01:50:16 -0400

    add new model and an initial python notebook demo code

---
+++ testing/python/config_reader.py
@@ -0,0 +1,24 @@
+from configobj import ConfigObj
+import numpy as np
+
+
+def config_reader():
+    config = ConfigObj('config')
+
+    param = config['param']
+    model_id = param['modelID']
+    model = config['models'][model_id]
+    model['boxsize'] = int(model['boxsize'])
+    model['np'] = int(model['np'])
+    num_limb = len(model['limbs'])/2
+    model['limbs'] = np.array(model['limbs']).reshape((num_limb, 2))
+    model['limbs'] = model['limbs'].astype(np.int)
+    param['starting_range'] = float(param['starting_range'])
+    param['ending_range'] = float(param['ending_range'])
+    param['octave'] = int(param['octave'])
+    param['use_gpu'] = int(param['use_gpu'])
+
+    return param, model
+
+if __name__ == "__main__":
+    config_reader()
