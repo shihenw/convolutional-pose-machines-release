12c200410cc9f92f249ba84d35d40907b0487208
shihenw <shihenw@cmu.edu>
2016-07-18 02:04:27 -0400

    update notebook

--- testing/python/CPM_demo.py
+++
@@ -1,9 +0,0 @@
-from config_reader import config_reader
-from applymodel import applymodel
-from visualize_save import visualize_save
-
-if __name__ == "__main__":
-    param, model = config_reader()
-    test_image = '../sample_image/im1429.jpg'
-    heatmaps, prediction = applymodel(test_image, param, model)
-    visualize_save(test_image, heatmaps, prediction, param, model) # save images
