0d9a428b4215d875fedbf5e607992a83350eeff6
shihenw <shihenw@cmu.edu>
2016-07-20 18:09:44 -0400

    ipython notebook demo explanation

--- testing/python/config_reader.py
+++ testing/python/config_reader.py
@@ -13,10 +13,12 @@ def config_reader():
     num_limb = len(model['limbs'])/2
     model['limbs'] = np.array(model['limbs']).reshape((num_limb, 2))
     model['limbs'] = model['limbs'].astype(np.int)
-    param['starting_range'] = float(param['starting_range'])
-    param['ending_range'] = float(param['ending_range'])
-    param['octave'] = int(param['octave'])
+    model['sigma'] = float(model['sigma'])
+    #param['starting_range'] = float(param['starting_range'])
+    #param['ending_range'] = float(param['ending_range'])
+    #param['octave'] = int(param['octave'])
     param['use_gpu'] = int(param['use_gpu'])
+    param['GPUdeviceNumber'] = int(param['GPUdeviceNumber'])
 
     return param, model
 
