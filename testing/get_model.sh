97795d4f2c86aecb62ef2f27a9755a5c23a78e36
Shih-En Wei <weisteady@gmail.com>
2018-06-05 12:39:51 -0400

    change file server

--- testing/get_model.sh
+++ testing/get_model.sh
@@ -1,7 +1,7 @@
-wget -nc --directory-prefix=../model/_trained_MPI/ 		http://pearl.vasc.ri.cmu.edu/caffe_model_github/model/_trained_MPI/pose_iter_630000.caffemodel
-wget -nc --directory-prefix=../model/_trained_MPI/              http://pearl.vasc.ri.cmu.edu/caffe_model_github/model/_trained_MPI/pose_iter_985000_addLEEDS.caffemodel
-wget -nc --directory-prefix=../model/_trained_LEEDS_PC/     	http://pearl.vasc.ri.cmu.edu/caffe_model_github/model/_trained_LEEDS_PC/pose_iter_395000.caffemodel
-wget -nc --directory-prefix=../model/_trained_FLIC/ 		http://pearl.vasc.ri.cmu.edu/caffe_model_github/model/_trained_FLIC/pose_iter_40000.caffemodel
+wget -nc --directory-prefix=../model/_trained_MPI/ 		http://posefs1.perception.cs.cmu.edu/CPM/caffe_model_github/model/_trained_MPI/pose_iter_630000.caffemodel
+wget -nc --directory-prefix=../model/_trained_MPI/              http://posefs1.perception.cs.cmu.edu/CPM/caffe_model_github/model/_trained_MPI/pose_iter_985000_addLEEDS.caffemodel
+wget -nc --directory-prefix=../model/_trained_LEEDS_PC/     	http://posefs1.perception.cs.cmu.edu/CPM/caffe_model_github/model/_trained_LEEDS_PC/pose_iter_395000.caffemodel
+wget -nc --directory-prefix=../model/_trained_FLIC/ 		http://posefs1.perception.cs.cmu.edu/CPM/caffe_model_github/model/_trained_FLIC/pose_iter_40000.caffemodel
 
-wget -nc --directory-prefix=../model/_trained_MPI/              http://pearl.vasc.ri.cmu.edu/caffe_model_github/model/_trained_MPI/pose_iter_320000.caffemodel
-wget -nc --directory-prefix=../model/_trained_person_MPI/       http://pearl.vasc.ri.cmu.edu/caffe_model_github/model/_trained_person_MPI/pose_iter_70000.caffemodel
+wget -nc --directory-prefix=../model/_trained_MPI/              http://posefs1.perception.cs.cmu.edu/CPM/caffe_model_github/model/_trained_MPI/pose_iter_320000.caffemodel
+wget -nc --directory-prefix=../model/_trained_person_MPI/       http://posefs1.perception.cs.cmu.edu/CPM/caffe_model_github/model/_trained_person_MPI/pose_iter_70000.caffemodel
